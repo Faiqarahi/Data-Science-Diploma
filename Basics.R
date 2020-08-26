fddad43d47d23361c85c3133fac5a60ef65e30d0
Faiqarahi <69055822+Faiqarahi@users.noreply.github.com>
2020-08-26 12:48:24 -0400

    Add files via upload

---
+++ Basics.R
@@ -0,0 +1,26 @@
+1+1
+x<-20
+y<-58
+z<-60
+x*y*z
+exp(x)
+pi
+#create vector
+x<-c(12,20,15,100)
+vec_x<-c(2,0,0,4)
+vec_y<-c(1,9,9,9)
+#add vectors
+vect_add <-vect_x+vect_y
+vec_x<-c(2,0,0,4)
+vec_y<-c(1,9,9,9)
+vect_add <-vec_x+vec_y
+vec_x+vec_y
+#Mul vec_x with some scalar
+vec_x*5
+#Accessing the elements
+vec_x[1]
+vec_x[4]
+vec_char<-c('R', 'Python', 'Java', 10, 10.5)
+vec_char
+df<-data.frame(x=1:4, y=c('a', 'b', 'c', 'd'))
+pnorm(70,60,10)
